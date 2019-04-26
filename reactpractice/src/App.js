d38abddbd0604229fa9caaf658584ed644be4ec7
laurenkerns <laurenkerns@gmail.com>
2019-04-26 15:07:36 -0400

    created react app

---
+++ reactpractice/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
+import Score from "./components/Score";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
